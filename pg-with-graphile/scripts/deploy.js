958453635ab0ace2202c0132418745ee8dbdf680
Brice Pelle <onlybakam@gmail.com>
2022-06-06 12:45:41 -0700

    add script to help deploy solution

--- pg-with-graphile/scripts/deploy.js
+++ pg-with-graphile/scripts/deploy.js
@@ -1,28 +1,40 @@
 const { spawn } = require('child_process')
 const arg = require('arg')
-const config = require('../../vpc-with-pg/output.json')
-
-// vpc info
-const vpcId = config.PgVpcStack.VPCID
-const sgId = config.PgRdsStack.LambdaSecurityGroupId
-
-// rds proxy info
-const dbProxyArn = config.PgRdsStack.RDSProxyARN
-const dbProxyName = config.PgRdsStack.RDSProxyName
-const endpoint = config.PgRdsStack.RDSProxyEndpoint
+const aws = require('aws-sdk')
 
 const args = arg({
+  '--region': String,
+  '--proxy': String,
+  '--sg': String,
   '--username': String,
   '--database': String,
   '--schemas': String,
 })
 
-const reqs = [('--username', '--database', '--schemas')]
-reqs.forEach((a) => {
-  if (!args[a]) throw new Error(`missing required argument: ${a}`)
-})
+async function run() {
+  try {
+    const reqs = [('--region', '--proxy', '--sg', '--username', '--database', '--schemas')]
+    reqs.forEach((a) => {
+      if (!args[a]) throw new Error(`missing required argument: ${a}`)
+    })
 
-const opts = `-c vpcId=${vpcId} \
+    const rds = new aws.RDS({ region: args['--region'] })
+
+    const result = await rds.describeDBProxies({ DBProxyName: args['--proxy'] }).promise()
+
+    const proxy = result.DBProxies[0]
+    if (!proxy) {
+      throw new Error('proxy not found')
+    }
+
+    const vpcId = proxy.VpcId
+    const sgId = args['--sg']
+
+    const dbProxyArn = proxy.DBProxyArn
+    const dbProxyName = proxy.DBProxyName
+    const endpoint = proxy.Endpoint
+
+    const opts = `-c vpcId=${vpcId} \
 --parameters sgId=${sgId} \
 --parameters dbProxyArn=${dbProxyArn} \
 --parameters dbProxyName=${dbProxyName} \
@@ -32,4 +44,12 @@ const opts = `-c vpcId=${vpcId} \
 --parameters schemas=${args['--schemas']} \
 -O output.json`.split(' ')
 
-const running = spawn('npm', ['run', 'cdk', 'deploy', '--', ...opts], { stdio: 'inherit' })
+    const running = spawn('npm', ['run', 'cdk', 'deploy', '--', ...opts], { stdio: 'inherit' })
+  } catch (error) {
+    return console.log('Error: ' + error.message)
+  }
+  console.log('Done')
+}
+
+console.log('Deploying...')
+run()
